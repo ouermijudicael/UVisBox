0b400af9bf3734a18232fe5ca35fb275c970d5b8
Jixian Li <jixianli@sci.utah.edu>
2025-08-20 14:03:02 -0600

    created first test. run pytest at the project root

---
+++ tests/unit/depths/test_contour_depths.py
@@ -0,0 +1,7 @@
+from uvisbox.BandDepths.contour_banddepth import choose2
+import math
+import numpy as np
+
+def test_choose2():
+    for i in range(2,100):
+        assert choose2(i) == math.comb(i, 2), f"choose2({i}) failed"
