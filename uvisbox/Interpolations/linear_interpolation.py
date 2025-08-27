ab74f24fb5c9b9f339129d2d27822caf45515b97
Jixian Li <jixianli@sci.utah.edu>
2025-08-27 16:23:07 -0600

    refactor linear interpolation to make the variable name clear

--- uvisbox/Interpolations/linear_interpolation.py
+++ uvisbox/Interpolations/linear_interpolation.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-def linear_interpolate(x, x_min, x_max, y_min, y_max):
+def linear_interpolate(in_value, in_min, in_max, out_min, out_max):
     """
     Performs linear interpolation to estimate an output value (`out`) corresponding to an input value (`in`).
 
     Parameters:
-        in (float): The input value for which to interpolate the output.
-        x_min (float): The minimum value of the input range.
-        x_max (float): The maximum value of the input range.
-        y_min (float): The minimum value of the output range.
-        y_max (float): The maximum value of the output range.
+        in_value (float): The input value for which to interpolate the output.
+        in_min (float): The minimum value of the input range.
+        in_max (float): The maximum value of the input range.
+        out_min (float): The minimum value of the output range.
+        out_max (float): The maximum value of the output range.
 
     Returns:
         float: The interpolated output value for the given input.
 
     Notes:
-        If `x_max` equals `x_min`, returns the average of `y_min` and `y_max` to avoid division by zero.
+        If `in_max` equals `in_min`, returns the average of `out_min` and `out_max` to avoid division by zero.
     """
-    if (x_max - x_min) == 0:
-        return (y_min + y_max) / 2
-    return y_min + (y_max - y_min) * (x - x_min) / (x_max - x_min)
+    if (in_max - in_min) == 0:
+        return (out_min + out_max) / 2
+    return out_min + (out_max - out_min) * (in_value - in_min) / (in_max - in_min)
