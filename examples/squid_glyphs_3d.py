e89ec790bdcd440158c3eb16109d594a49f57557
ouermijudicael <ouermijudicael@gmail.com>
2025-08-28 17:23:30 -0600

    removed playground

--- examples/squid_glyphs_3d.py
+++ examples/squid_glyphs_3d.py
@@ -3,7 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from uvisbox.Glyphs.squid_glyph import cartesian_to_spherical, compute_vector_depths_3D, uncertainty_squid_glyphs_3D
 
 def generate_3d_grid_vectors():
-    grid_size = 2
+    grid_size = 10
     vectors = []
     for x in range(grid_size):
         for y in range(grid_size):
