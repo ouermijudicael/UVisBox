8e6f1347af1efb15dfda7fa02b3bd6897dc32fa5
ouermijudicael <ouermijudicael@gmail.com>
2025-09-15 16:02:19 -0600

    update config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 project = 'UVisBox'
 copyright = '2025, Timbwaoga A. J. Ouermi and Jixian Li'
