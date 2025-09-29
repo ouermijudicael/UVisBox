6bdc5226afee321ef4bfe4d1bca3481f613f0874
ouermijudicael <ouermijudicael@gmail.com>
2025-09-29 10:42:19 -0600

    Enhance documentation for probabilistic_marching_triangles function with reference to relevant research

--- uvisbox/ProbabilisticContours/Stat/probabilistic_marching_triangles.py
+++ uvisbox/ProbabilisticContours/Stat/probabilistic_marching_triangles.py
@@ -2,9 +2,15 @@ import numpy as np
 
 def probabilistic_marching_triangles(F, triangles, isovalue, num_samples=200):
     """
-    Perform probabilistic marching triangles on a 2D scalar field with uncertainty. This function calculates
-    the probability of the isocontour passing through each triangle in the triangulated mesh based on an ensemble of scalar fields.
- 
+    Perform probabilistic marching triangles on a 2D scalar field with uncertainty. 
+    This function calculatesthe probability of the isocontour passing through each 
+    triangle in the triangulated mesh based on an ensemble of scalar fields.
+    
+    The method is based on the paper: Pöthkow, K., Petz, C. and Hege, H.C., 2013. 
+    Approximate level-crossing probabilities for interactive visualization of uncertain 
+    isocontours. International Journal for Uncertainty Quantification, 3(2).
+    doi: 10.1615/Int.J.UncertaintyQuantification.2012003958
+
     Parameters:
     -----------
         F : np.ndarray
@@ -15,6 +21,7 @@ def probabilistic_marching_triangles(F, triangles, isovalue, num_samples=200):
             The isovalue for which to compute the contour.  
         num_samples : int, optional
             Number of samples to draw for estimating the probability of contour presence in each cell.
+    
     Returns:
     --------
         prob_contour : np.ndarray
