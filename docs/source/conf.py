430a2c6f23cf8f08119065bf309af486617811a8
ouermijudicael <ouermijudicael@gmail.com>
2025-10-30 16:00:55 -0600

    Fix formatting in Sphinx configuration file by removing extra newline

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,7 @@ author = 'Timbwaoga A. J. Ouermi and Jixian Li'
 release = '0.0.1'
 
 
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
