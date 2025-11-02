0db314020e4bbcfe72ee260192c80ef4a2860f5d
Jixian Li <jixianli@sci.utah.edu>
2025-11-02 12:31:38 -0700

    refactor functional boxplot

--- uvisbox/Modules/FunctionalBoxplot/__init__.py
+++ uvisbox/Modules/FunctionalBoxplot/__init__.py
@@ -6,11 +6,28 @@ This module provides functional boxplot functionality for uncertainty visualizat
 
 # Import meshing and visualization functionality
 try:
-    from .functional_boxplot_mesh import *
-    from .functional_boxplot_stats import *
-    from .functional_boxplot_vis import *
-    from uvisbox.Modules.FunctionalBoxplot.functional_boxplot import *
+    from .functional_boxplot_mesh import functional_boxplot_mesh
+    from .functional_boxplot_stats import (
+        band_depths,
+        get_band,
+        summary_statistics
+    )
+    from .functional_boxplot_vis import (
+        plot_band,
+        visualize_functional_boxplot
+    )
+    from .functional_boxplot import functional_boxplot
 except ImportError:
     pass
+
+__all__ = [
+    'functional_boxplot_mesh',
+    'band_depths',
+    'get_band',
+    'summary_statistics',
+    'plot_band',
+    'visualize_functional_boxplot',
+    'functional_boxplot'
+]
 
 # Stats subdirectory is available for future expansion
